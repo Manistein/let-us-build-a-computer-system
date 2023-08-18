214dc99084ca4415707a5534edef3d3456a8c90d
Manistein <1518986465@qq.com>
2023-08-18 15:52:07 +0800

    Update the hCPU test case.

--- hardware/HDL/computer/hCPU.v
+++ hardware/HDL/computer/hCPU.v
@@ -126,7 +126,7 @@ module hCPU(
 	 wire [2:0] dest; // c-instruction dest part;
 	 wire [2:0] jump; // c-instruction jump part;
 	 reg [15:0] reg_a, reg_d;
-	 reg [15:0] reg_a_next, reg_d_next;
+	 wire [15:0] reg_a_next, reg_d_next;
 	 wire [15:0] pc_next, to_hpc;
 	 wire enable_load_pc, enable_inc_pc;
 	 
@@ -158,6 +158,7 @@ module hCPU(
 	 always @(posedge clock) begin
 		if (msb == 0) begin
 			reg_a <= instruction[14:0];
+			reg_d <= reg_d_next;
 		end else begin
 			if (dest[2] == 1'b1) begin
 				reg_a <= alu_out;
@@ -171,10 +172,9 @@ module hCPU(
 				reg_d <= reg_d_next;
 			end
 		end
-		
-		reg_a_next = reg_a;
-		reg_d_next = reg_d;
 	 end
+	 assign reg_a_next = reg_a;
+	 assign reg_d_next = reg_d;
 	 
 	 hPC hpc(.in(to_hpc), .load(enable_load_pc), .inc(enable_inc_pc), .reset(reset), .clock(clock), .out(pc_next));
 	 
