0620882ec75b135065f5c8400c178113f68e8f86
Manistein <1518986465@qq.com>
2024-04-26 18:27:51 +0800

    Update hub.v

---
+++ hardware/HDL/z0-computer/cpu/hub.v
@@ -0,0 +1,16 @@
+`ifndef _hub_v_ 
+`define _hub_v_
+
+module hub(
+    input [15:0] instruction,
+    input [15:0] pc_in,
+    input is_loaded,
+
+    output [15:0] pc_out
+);
+
+
+
+endmodule
+
+`endif
